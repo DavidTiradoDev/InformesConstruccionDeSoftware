923a2c413f619942f0a6aa442250bef3909f476b
juan pablo garcia <juanpagarcia17@hotmail.com>
2026-05-07 18:50:44 -0500

    agrega condicionales simples

---
+++ Taller2/src/Ejercicios/CondicionalSimple.java
@@ -0,0 +1,17 @@
+package Ejercicios;
+
+public class CondicionalSimple {
+
+	public static void main(String[] args) {
+		//condicionales simples
+		int a = 5;
+		int b = 6;
+		
+		if (a < b) {
+			System.out.println(a + " es menor que " + b);
+		}
+		
+
+	}
+
+}
